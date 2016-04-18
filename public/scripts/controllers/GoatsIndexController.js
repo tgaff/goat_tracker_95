b1b3a35dc8334d4d55b30c43ef63e768e829a5d8
tgaff <tgaff@alumni.nd.edu>
2016-04-18 09:02:01 -0700

    tear down index

--- public/scripts/controllers/GoatsIndexController.js
+++ public/scripts/controllers/GoatsIndexController.js
@@ -10,14 +10,7 @@ function GoatsIndexController(   $http   ) {
   fetchAllGoats();  // fetch goats on start
 
   function fetchAllGoats() {
-    $http({
-      method: 'GET',
-      url: baseUrl + '/api/goats'
-    }).then(function successCallback(response) {
-      console.log('goats received: ', response);
-      vm.goats = response.data;
-    }, function errorCallback(response) {
-      console.log('There was an error getting the data', response);
-    });
+    // use $http to get baseUrl + /goats
+    // store them in vm.goats
   }
 }
